8ec253f1c8f15fa8cbfb52bbf57792bdd921ab1d
adkood <ashutoshnegi195@gmail.com>
2024-05-02 01:08:08 +0530

    added cluster

--- app.js
+++ app.js
@@ -34,16 +34,14 @@ app.use('/api', transactionRoutes);
 // global error handling 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Worker ${process.pid} is listening at port ${PORT}...`);
-});
-
-// startCluster(() => {
-//     const PORT = process.env.PORT || 5000;
-
-//     app.listen(PORT, () => {
-//         console.log(`Worker ${process.pid} is listening at port ${PORT}...`);
-//     });
+// app.get('/', (req, res) => {
+//     res.send(`processId: ${process.pid}`);
 // });
+
+startCluster(() => {
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+        console.log(`Worker ${process.pid} is listening at port ${PORT}...`);
+    });
+});
