b6140a875b38a18656b2dbea6151f2ec33df29ab
adkood <ashutoshnegi195@gmail.com>
2024-05-02 01:53:36 +0530

    removed depricated headers

--- db.js
+++ db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         console.log(process.env.DB_KEY);
-        await mongoose.connect(process.env.DB_KEY.replace('<password>', process.env.DB_PASSWORD), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        await mongoose.connect(process.env.DB_KEY.replace('<password>', process.env.DB_PASSWORD))
         console.log('connected to mongoDb');
     } catch(err) {
         console.log(err);
