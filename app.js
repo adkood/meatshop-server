f39cfab4c5070132f5f9fced6e19da276bded0b4
adkood <ashutoshnegi195@gmail.com>
2024-05-02 00:51:49 +0530

    removed cluster

--- app.js
+++ app.js
@@ -34,14 +34,16 @@ app.use('/api', transactionRoutes);
 // global error handling 
 app.use(errorHandler);
 
-app.get('/', (req, res) => {
-    res.send(`processId: ${process.pid}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Worker ${process.pid} is listening at port ${PORT}...`);
 });
 
-startCluster(() => {
-    const PORT = process.env.PORT || 5000;
+// startCluster(() => {
+//     const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, () => {
-        console.log(`Worker ${process.pid} is listening at port ${PORT}...`);
-    });
-});
+//     app.listen(PORT, () => {
+//         console.log(`Worker ${process.pid} is listening at port ${PORT}...`);
+//     });
+// });
