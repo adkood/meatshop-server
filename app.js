61cd37c14d2436de4debfb01addbb7cd88beebd9
adkood <ashutoshnegi195@gmail.com>
2024-05-02 23:25:32 +0530

    trying to add cluster

--- app.js
+++ app.js
@@ -34,16 +34,11 @@ app.use('/api', transactionRoutes);
 // global error handling 
 app.use(errorHandler);
 
-// app.get('/', (req, res) => {
-//     res.send(`processId: ${process.pid}`);
-// });
+startCluster(() => {
+    const PORT = process.env.PORT || 5000;
 
-// startCluster(() => {
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Worker ${process.pid} is listening at port ${PORT}...`);
+    app.listen(PORT, () => {
+        console.log(`Worker ${process.pid} is listening at port ${PORT}...`);
+    });
 });
-// });
 
-module.exports = app;
--- utils/clusterManager.js
+++ utils/clusterManager.js
@@ -1,19 +1,32 @@
 const cluster = require('cluster');
 const os = require('os');
 
+const MAX_RESTARTS = 5;
+
 function startCluster(appCallback) {
     if (cluster.isPrimary) {
         const numCPUs = os.cpus().length;
         for (let i = 0; i < numCPUs; i++) {
-            cluster.fork();
+            createWorker();
         }
+        cluster.on('exit', (worker, code, signal) => {
+            if (worker.restarts < MAX_RESTARTS) {
+                console.log(`Worker ${worker.process.pid} died. Restarting...`);
+                createWorker();
+            } else {
+                console.log(`Worker ${worker.process.pid} reached maximum restart limit. Not restarting.`);
+            }
+        });
     } else {
         appCallback();
     }
+}
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
-        cluster.fork();
+function createWorker() {
+    const worker = cluster.fork();
+    worker.restarts = 0;
+    worker.on('exit', (code, signal) => {
+        worker.restarts++;
     });
 }
 
